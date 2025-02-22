d38eef263ca2f0e4b183190d58025f02709bd2ab
canxin <q1969730106@gmail.com>
2025-02-23 02:28:57 +0800

    update gradle version, remove Registrar

--- android/src/main/java/com/zt/shareextend/ShareExtendPlugin.java
+++ android/src/main/java/com/zt/shareextend/ShareExtendPlugin.java
@@ -11,7 +11,6 @@ import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.PluginRegistry;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /**
  * Plugin method host for presenting a share sheet via Intent
@@ -29,25 +28,24 @@ public class ShareExtendPlugin implements FlutterPlugin, ActivityAware, PluginRe
     private MethodCallHandlerImpl callHandler;
     private Share share;
 
-    public static void registerWith(Registrar registrar) {
-        ShareExtendPlugin plugin = new ShareExtendPlugin();
-        plugin.setUpChannel(registrar.context(), registrar.messenger(), registrar, null);
-    }
-
     @Override
     public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
         pluginBinding = flutterPluginBinding;
     }
 
     @Override
-    public void onDetachedFromEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        if (methodChannel != null) {
+            methodChannel.setMethodCallHandler(null);
+            methodChannel = null;
+        }
         pluginBinding = null;
     }
 
     @Override
     public void onAttachedToActivity(@NonNull ActivityPluginBinding activityPluginBinding) {
         activityBinding = activityPluginBinding;
-        setUpChannel(activityBinding.getActivity(), pluginBinding.getBinaryMessenger(), null, activityBinding);
+        setUpChannel(activityBinding.getActivity(), pluginBinding.getBinaryMessenger(), activityBinding);
     }
 
     @Override
@@ -65,23 +63,23 @@ public class ShareExtendPlugin implements FlutterPlugin, ActivityAware, PluginRe
         tearDown();
     }
 
-    private void setUpChannel(Context context, BinaryMessenger messenger,Registrar registrar, ActivityPluginBinding activityBinding) {
+    private void setUpChannel(Context context, BinaryMessenger messenger, ActivityPluginBinding activityBinding) {
         methodChannel = new MethodChannel(messenger, CHANNEL);
         share = new Share(context);
         callHandler = new MethodCallHandlerImpl(share);
         methodChannel.setMethodCallHandler(callHandler);
-        if (registrar != null) {
-            registrar.addRequestPermissionsResultListener(this);
-        } else {
-            activityBinding.addRequestPermissionsResultListener(this);
-        }
+        activityBinding.addRequestPermissionsResultListener(this);
     }
 
     private void tearDown() {
-        activityBinding.removeRequestPermissionsResultListener(this);
-        activityBinding = null;
-        methodChannel.setMethodCallHandler(null);
-        methodChannel = null;
+        if (activityBinding != null) { // 避免空指针异常
+            activityBinding.removeRequestPermissionsResultListener(this);
+            activityBinding = null;
+        }
+        if (methodChannel != null) {
+            methodChannel.setMethodCallHandler(null);
+            methodChannel = null;
+        }
     }
 
     @Override
